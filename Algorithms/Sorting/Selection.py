8e8ec632ef72f87f2c0b466b0fe80983f07c1a70
Park YengSeo <1park4170@gmail.com>
2022-05-11 17:28:14 +0900

    remove obsolete line

--- Algorithms/Sorting/Selection.py
+++ Algorithms/Sorting/Selection.py
@@ -1,12 +1,10 @@
 arr = [5,9,1,2,7,3,8,2]
 
 def selectionsort(array):
-    count = 0
     for i in range(len(array)-1):
         minn = array[i]
         index = i
         for j in range(i+1, len(array)):
-            count += 1
             if array[j] < minn :
                 index = j
                 minn = array[j]
